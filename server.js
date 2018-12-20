141a3f9f660799071d072e6982777502e51a7d0d
CAO, Xiaoyi <x9cao@eng.ucsd.edu>
2018-12-19 23:37:04 -0800

    Update server.js to send notification emails

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const multer = require('multer')
 const fs = require('fs');
+const nodemailer = require('nodemailer')
 const upload = multer({ dest: 'uploads/' })
 const spawn = require('child_process').spawn
 // express.js
@@ -39,7 +40,7 @@ app.post('/form_upload', cpUpload, function (req, res) {
       var runid = makeid()
       tmp_stat = fs.statSync("tmp_" + runid)
     }
-    catch(err){
+    catch (err) {
       // if the folder does not exist
       console.log("Just created a new directory: " + runid)
       fs.mkdirSync("tmp_" + runid)
@@ -48,9 +49,9 @@ app.post('/form_upload', cpUpload, function (req, res) {
     }
   }
   while (flag === 1)
- 
+
   // construct an object for python input
-  var pyMessenger = {'body': req.body, 'files': req.files, 'runid': "tmp_" + runid}
+  var pyMessenger = { 'body': req.body, 'files': req.files, 'runid': "tmp_" + runid }
   var stdoutData = ''
   var stderrData = ''
 
@@ -59,8 +60,8 @@ app.post('/form_upload', cpUpload, function (req, res) {
 
   // Handle normal output
   scriptExecution.stdout.on('data', (data) => {
-      stdoutData += data
-      console.log(data + '')
+    stdoutData += data
+    console.log(data + '')
   })
 
   // Handle error output
@@ -70,10 +71,56 @@ app.post('/form_upload', cpUpload, function (req, res) {
   })
 
   scriptExecution.on('exit', (code) => {
+    let message = null
+    if (req.body.emailNote && req.body.mail) {
+      message = {
+        from: 'no-reply@mail.beta.givengine.org',
+        to: req.body.mail
+      }
+    }
+    let header = 'Dear user,\n\n'
+    let footer = '\n\nEpiAlignment Team\n--\nZhong Lab\n' +
+      'Department of Bioengineering, Mail Code 0412\n' +
+      'University of California, San Diego\n' +
+      '9500 Gilman Dr.\nLa Jolla, CA 92122-0412\nUnited States'
+
+    if (message) {
       console.log("Process quit with code : " + code)
-      console.log(stdoutData)
-      console.log(stderrData)
       RunID_dict[runid] = code
+      // needs to write an email
+      let transporter = nodemailer.createTransport({
+        sendmail: true,
+        newline: 'unix',
+        path: '/usr/sbin/sendmail'
+      })
+      if (code) {
+        // some kind of error has happened
+        message.subject = 'Error when computing your EpiAlignment data'
+        message.text = header +
+          'Unfortunately, an error occured when processing ' +
+          'your data for EpiAlignment (Error Code: ' + code +
+          '). We are unable to complete your request. \n\n' +
+          'In some cases this may be due to an erroneous input format, ' +
+          'in which case you may try again by providing the correctly ' +
+          'formatted input at https://beta.epialign.ucsd.edu/.\n\n' +
+          'If the error keep happening, please let us know by emailing ' +
+          'Jia Lu<jil430@eng.ucsd.edu> or Xiaoyi Cao <x9cao@eng.ucsd.edu>. ' +
+          'Thank you for trying EpiAlignment!' + footer
+      } else {
+        // Normal email
+        message.subject = 'Your EpiAlignment results are ready'
+        message.text = header +
+          'Your EpiAlignment results are now ready and can be downloaded ' +
+          'by following this link:\n\n' +
+          'https://beta.epialign.ucsd.edu/result_page/' + runid + '\n\n' +
+          'If the link above does not work, please copy the entire link and' +
+          'paste it into the address bar of your web browser.\n\n'
+        'If you have any questions, please let us know by emailing ' +
+          'Jia Lu<jil430@eng.ucsd.edu> or Xiaoyi Cao <x9cao@eng.ucsd.edu>. ' +
+          'Thank you for trying EpiAlignment!' + footer
+      }
+      transporter.sendMail(message)
+    }
   });
   // console.log(JSON.stringify(pyMessenger))
   // python input
@@ -81,11 +128,11 @@ app.post('/form_upload', cpUpload, function (req, res) {
   // tell the node that sending inputs to python is done.
   scriptExecution.stdin.end();
 
-  res.json(req.files)
+  res.json({ runid: runid })
 
 })
 
-app.get('/download_results/:runid', function(req, res){
+app.get('/download_results/:runid', function (req, res) {
   var runid = req.params.runid
   console.log(runid)
   console.log(RunID_dict)
@@ -107,10 +154,10 @@ app.get('/download_results/:runid', function(req, res){
 });
 
 const server = app.listen(3000, function () {
- 
+
   var host = server.address().address
   var port = server.address().port
- 
+
   console.log('Listening http://%s:%s', host, port)
- 
+
 })

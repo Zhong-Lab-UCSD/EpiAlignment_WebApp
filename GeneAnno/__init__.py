c0d6d524ccd86d7f8832874579562f65618b0117
Jia Lu <jil340@eng.ucsd.edu>
2019-01-10 22:02:42 -0800

    Sample substitution
    EpiAlignment: 0.5*diag_norm
    Merge overlapping promoter regions
    Remove sequence weight

--- GeneAnno/__init__.py
+++ GeneAnno/__init__.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 class Transcripts:
   def __init__(self, x):
     '''
@@ -44,7 +46,7 @@ def Construct_nameDict(fname):
 
 def PromoterBed(trans_obj, cutUp = 0, cutDown = 0):
   '''
-  Cut the promoter regions and return a bed 6 list. All elements are strings.
+  Cut the promoter regions and return a bed 6 list.
   '''
   if trans_obj.strand == "+":
     start = trans_obj.start - cutUp
@@ -52,7 +54,50 @@ def PromoterBed(trans_obj, cutUp = 0, cutDown = 0):
   else:
     start = trans_obj.stop - cutDown
     stop = trans_obj.stop + cutUp
-  return [trans_obj.chr, str(start), str(stop), trans_obj.ensid + "_" + trans_obj.transname, '0', trans_obj.strand]
+  return [trans_obj.chr, start, stop, trans_obj.ensid + "_" + trans_obj.name, '0', trans_obj.strand]
+
+def Dist_overlap(region1, region2, N):
+  start1 = region1[1]
+  stop1 = region1[2]
+  start2 = region2[1]
+  stop2 = region2[2]
+
+  if min((stop1 - start2), (stop2 - start1)) >= N:
+    return True
+  else:
+    return False
+
+
+def PromoterMerge(gene, trans_dict, cutUp = 0, cutDown = 0):
+  '''
+  Merge overlapping promoters of the same gene.
+  '''
+  N = float(cutUp + cutDown) * 0.8
+  res_list = []
+  if gene not in trans_dict:
+    return res_list
+  promoter_list = [PromoterBed(trans_obj, cutUp, cutDown) for trans_obj in trans_dict[gene]]
+  # sort tmp_list by chromosome names and starts.
+  promoter_list_sort = sorted(promoter_list, key=lambda x: x[1]))
+  for p in promoter_list_sort:
+    if len(res_list) == 0:
+      res_list.append(deepcopy(p))
+    if overlap(p, res_list[-1], N):
+      # Update start
+      res_list[-1][1] = min(p[1], res_list[-1][1])
+      # Update stop
+      res_list[-1][2] = max(p[2], res_list[-1][2])
+    else:
+      # change region name
+      res_list[-1][1] = str(res_list[-1][1])
+      res_list[-1][2] = str(res_list[-1][2])
+      res_list[-1][3] += res_list[-1][1][-4:]
+      res_list.append(deepcopy(p))
+  # The last one
+  res_list[-1][1] = str(res_list[-1][1])
+  res_list[-1][2] = str(res_list[-1][2])
+  res_list[-1][3] += res_list[-1][1][-4:]
+  return res_list
 
 
 
